74152e04ce42f1f072d976795aac1fb9aad6eb46
Dart2.0 <59048496+Dart2004@users.noreply.github.com>
2022-10-10 12:03:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Daniel Klimmer',
     author_email='danielklimmer2000@protonmail.com',
     packages=['splitticai'],
-    install_requires=['requests', 'bs4'],
+    install_requires=['requests', 'bs4', 'pypandoc'],
     version='1.0',
     license='MIT',
     description='The Most Advanced AI Ever',
